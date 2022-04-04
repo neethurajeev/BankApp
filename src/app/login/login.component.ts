d30b7993c36e6bd49e3c0edfcb3c5dd07442463d
neethu <neethurajeev3@gmail.com>
2022-04-04 23:51:09 +0530

    adding binding data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,10 +10,10 @@ aim="THANK YOU FOR CHOOSING HDFC"
 accnum="Account Number Please!!!"
 acno=""
 pwd=""
-database={
-  1000:{accno:1000,unmae:"neethu",password:1000,balance:5000},
-  1001:{accno:1001,uname:"anoop",password:1001,balance:4000},
-  1002:{accno:1002,uname:"abhi",password:1002,balance:3000}
+database:any={
+  1000:{acno:1000,uname:"neethu",password:1000,balance:5000},
+  1001:{acno:1001,uname:"anoop",password:1001,balance:4000},
+  1002:{acno:1002,uname:"abhi",password:1002,balance:3000}
 }
   constructor() { }
 
@@ -30,7 +30,23 @@ database={
     
   }
   login(){
-    alert("Login Clicked!!!!")
+    // alert("Login Clicked!!!!")
+    var acno=this.acno
+    var pwd=this.pwd
+    let database=this.database
+    if(acno in database){
+      if(pwd==database[acno]["password"])
+      {
+        alert("Login success")
+      }
+      else{
+        alert("Invalid password")
+      }
+
+    }
+    else{
+      alert("Invalid user")
+    }
   }
 
 }
