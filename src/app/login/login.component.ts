b245b5000c1b4ff58bc59881cd226c2107fe75e8
neethu <neethurajeev3@gmail.com>
2022-04-05 11:54:54 +0530

    Dependancy

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 aim="THANK YOU FOR CHOOSING HDFC"
 accnum="Account Number Please!!!"
+pswd="Password"
 acno=""
 pwd=""
+// database
 database:any={
   1000:{acno:1000,uname:"neethu",password:1000,balance:5000},
   1001:{acno:1001,uname:"anoop",password:1001,balance:4000},
   1002:{acno:1002,uname:"abhi",password:1002,balance:3000}
 }
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -38,6 +41,7 @@ database:any={
       if(pwd==database[acno]["password"])
       {
         alert("Login success")
+        this.router.navigateByUrl("dashboard")
       }
       else{
         alert("Invalid password")
@@ -49,4 +53,26 @@ database:any={
     }
   }
 
-}
+
+// login(a:any,p:any){
+//       console.log(a.value);
+      
+//       var acno=a.value
+//       var pwd=p.value
+//       let database=this.database
+//       if(acno in database){
+//         if(pwd==database[acno]["password"])
+//         {
+//           alert("Login success")
+//         }
+//         else{
+//           alert("Invalid password")
+//         }
+  
+//       }
+//       else{
+//         alert("Invalid user")
+//       }
+// }
+//}
+} 
